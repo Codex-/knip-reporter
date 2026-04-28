add08112d3485a176818c28e6dcf16f3942000ae
Alex Miller <codex.nz@gmail.com>
2026-04-28 21:30:04 +1200

    chore: remove no longer needed knip config

--- knip.ts
+++
@@ -1,9 +0,0 @@
-import type { KnipConfig } from "knip";
-
-const config: KnipConfig = {
-  ignore: ["dist/**"],
-  // Both used in eslint.config.mjs
-  ignoreDependencies: ["eslint-import-resolver-typescript"],
-};
-
-export default config;
