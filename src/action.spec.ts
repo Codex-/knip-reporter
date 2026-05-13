fb1690e170e697d17a6274ed9aa5e13681e9e5fa
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-05-13 19:25:48 +0100

    feat: run from existing report

--- src/action.spec.ts
+++ src/action.spec.ts
@@ -33,6 +33,7 @@ describe("Action", () => {
       verbose: actionInputs.verbose?.default,
       ignore_results: actionInputs.ignore_results?.default,
       working_directory: actionInputs.working_directory?.default,
+      output_json_file: actionInputs.output_json_file?.default,
     };
 
     vi.spyOn(core, "getInput").mockImplementation((input: string) => {
@@ -41,6 +42,7 @@ describe("Action", () => {
         case "command_script_name":
         case "comment_id":
         case "working_directory":
+        case "output_json_file":
           // eslint-disable-next-line @typescript-eslint/no-unsafe-return
           return mockEnvConfig[input];
         default:
@@ -76,6 +78,7 @@ describe("Action", () => {
       );
       expect(config.ignoreResults).toStrictEqual(actionInputs.ignore_results?.default);
       expect(config.workingDirectory).toStrictEqual(actionInputs.working_directory?.default);
+      expect(config.outputJsonFile).toStrictEqual(actionInputs.output_json_file?.default);
     });
 
     describe("custom values", () => {
